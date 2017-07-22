727fa56205d7aee8664b64d895b07352d80deb58
JR <jr@gongevangen.dk>
2017-07-22 10:36:42 +0200

    Projects page added

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'List of projects',
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
